83c5403c6396538afa4c963c3980aa9705417c59
Susana Natali Juan Madriz <sjuan-ma@c1r11s1.42madrid.com>
2024-07-24 18:36:01 +0200

    empezando

--- arguments.c
+++ arguments.c
@@ -2,25 +2,6 @@
 
 char *prueba(int fd);
 
-/* int main(int argc, char **argv)
-{
-	int fd;
-	char *line;
-
-	// fd = open("she_dont_give_a_fo", O_RDONLY);
-	fd = open(argv[1], O_RDONLY);
-	if(fd == -1)
-	{
-		printf("Error al abrir el archivo");
-		return 1;
-	}
-	line = prueba(fd);
-	printf("1RA LINEA%s\n", line);
-	free(line);
-	close(fd);
-	return 0;
-} */
-
 int	main(int argc, char **argv)
 {
 	int		fd;
