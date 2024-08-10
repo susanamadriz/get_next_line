aacc974557a2094bc41c11facae35cdeefc79598
Susana Natali Juan Madriz <sjuan-ma@c1r6s6.42madrid.com>
2024-08-10 17:08:21 +0200

    Limpianding gnl

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,5 +1,3 @@
-//funciones que puedes usar son (ft_strjoin) (ft_substr) (ft_strlen) (ft_strchr) (ft_strdup)
-//animo ;) ! i will stay here
 #include "get_next_line.h"
 
 void	*ft_calloc(size_t count, size_t size)
