5e495f9f6e450f673910de70d594b9f123cfcf9c
Susana Madriz <139632743+susanamadriz@users.noreply.github.com>
2024-08-10 20:44:25 +0000

    VAMOS NOJODAAAAAAAAA

--- get_next_line.c
+++ get_next_line.c
@@ -1,149 +1,108 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test.c                                             :+:      :+:    :+:   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sjuan-ma <sjuan-ma@student.42.fr>          +#+  +:+       +#+        */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 17:02:33 by sjuan-ma          #+#    #+#             */
-/*   Updated: 2024/08/08 21:34:12 by sjuan-ma         ###   ########.fr       */
+/*   Updated: 2024/08/10 20:43:49 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char *more_info(char *save, int fd)
+void	*ft_calloc(size_t count, size_t size)
 {
-    ssize_t bytes_read = 1;
-    char *buf, *aux, *raw = ft_substr(save, 0, ft_strlen(save));
+	size_t	total_size;
+	char	*pointer;
 
-    while (bytes_read != 0 && (!ft_strchr(raw, '\n')))
-    {
-        if (!(buf = ft_calloc(sizeof(char), (BUFFER_SIZE + 1))))
-        {
-            free(raw);
-            return (NULL);
-        }
-        if ((bytes_read = read(fd, buf, BUFFER_SIZE)) < 0)
-        {
-            free(buf);
-            free(raw);
-            return (NULL);
-        }
-        aux = raw;
-        raw = ft_strjoin(aux, buf);
-        free(aux);
-        free(buf);
-    }
-    return (raw);
+	total_size = count * size;
+	if (size != 0 && total_size >= SIZE_MAX)
+		return (NULL);
+	pointer = malloc(total_size);
+	if (pointer == NULL)
+		return (NULL);
+	while (total_size > 0)
+	{
+		pointer[total_size - 1] = 0;
+		total_size--;
+	}
+	return (pointer);
 }
 
-char *cut_line(char *raw)
+char	*more_info(char *save, int fd)
 {
-	char *result;
+	ssize_t	bytes_read;
+	char	*buf;
+	char	*aux;
+	char	*raw;
+
+	raw = ft_substr(save, 0, ft_strlen(save));
+	bytes_read = 1;
+	while (bytes_read != 0 && !ft_strchr(raw, '\n'))
+	{
+		buf = ft_calloc(sizeof(char), (BUFFER_SIZE + 1));
+		if (!buf)
+			return (free (raw), (NULL));
+		bytes_read = read(fd, buf, BUFFER_SIZE);
+		if (bytes_read < 0)
+			return (free(buf), free(raw), (NULL));
+		aux = raw;
+		raw = ft_strjoin(aux, buf);
+		free(aux);
+		free(buf);
+	}
+	return (raw);
+}
+
+char	*cut_line(char *raw)
+{
+	char	*result;
 
 	if (raw && ft_strchr(raw, '\n'))
-		result = ft_substr(raw, 0, (ft_strlen(raw) - ft_strlen(ft_strchr(raw, '\n') + 1)));
+		result = ft_substr(raw, 0, (ft_strlen(raw)
+					- ft_strlen(ft_strchr(raw, '\n') + 1)));
 	else
 		result = ft_strdup(raw);
 	return (result);
 }
 
-char *update_save(char *raw)
+char	*update_save(char *raw)
 {
-	char *save;
-	char *aux;
+	char	*save;
+	char	*aux;
 
 	aux = ft_strchr(raw, '\n');
 	if (aux)
-		save = ft_substr(aux, 1, ft_strlen(aux));  // El caso en que teneomos linea nueva.  No estamos al finaal
+		save = ft_substr(aux, 1, ft_strlen(aux));
 	else
-			// si estamos al final de la fichera
 		return (NULL);
 	return (save);
 }
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-    char *newstr;
-    char *raw;
-    static char *save = NULL;
+	char		*newstr;
+	char		*raw;
+	static char	*save = NULL;
 
-    raw = more_info(save, fd);
-    free(save);
-    if (!raw)
-        return (NULL);
-    if (raw[0] == 0)
-    {
-        free(raw);
-        return(NULL);
-    }
-    newstr = cut_line(raw);
-    if (!newstr)
-    {
-        free(raw);
-        return NULL;
-    }
-    save = update_save(raw);
-    free(raw);	
-    return (newstr);
-}
-
-#include "get_next_line.h"
-#include <fcntl.h>
-#include <stdio.h>
-
-// int	main(int argc, char **argv)
-// {
-// 	int		fd;
-// 	char	*str;
-// 	int i = 0;
-
-// 	if (argc != 2)
-// 	{
-// 		printf("Usage: %s <filename>\n", argv[0]);
-// 		return (1);
-// 	}
-// 	fd = open(argv[1], O_RDONLY);
-// 	if (fd == -1)
-// 	{
-// 		perror("Error opening file");
-// 		return (1);
-// 	}
-
-// //	str = get_next_line(fd);
-// 	while (i < 1)
-// 	{
-// 		str = get_next_line(fd);
-// 		printf("%s", str);
-// 		free(str);
-// 		i++;
-// 	}
-// 	close(fd);
-// 	return (0);
-// }
-
-int main(int argc, char **argv)
-{
-    int fd;
-    char *line;
-
-    if (argc != 2)
-    {
-        printf("Usage: %s <filename>\n", argv[0]);
-        return (1);
-    }
-    fd = open(argv[1], O_RDONLY);
-    if (fd == -1)
-    {
-        perror("Error opening file");
-        return (1);
-    }
-    while ((line = get_next_line(fd)) != NULL)
-    {
-        printf("%s\n", line);
-        free(line);
-    }
-    close(fd);
-    return (0);
+	raw = more_info(save, fd);
+	free (save);
+	if (!raw)
+		return (NULL);
+	if (raw[0] == 0)
+	{
+		free(raw);
+		return (NULL);
+	}
+	newstr = cut_line(raw);
+	if (!newstr)
+	{
+		free (raw);
+		return (NULL);
+	}
+	save = update_save(raw);
+	free (raw);
+	return (newstr);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,29 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/11 17:02:33 by sjuan-ma          #+#    #+#             */
+/*   Updated: 2024/08/10 20:41:15 by codespace        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
-
-void	*ft_calloc(size_t count, size_t size)
-{
-    size_t	total_size;
-    char	*pointer;
-
-    total_size = count * size;
-    if (size != 0 && total_size >= SIZE_MAX)
-        return (NULL);
-    pointer = malloc(total_size);
-    if (pointer == NULL)
-        return (NULL);
-    while (total_size > 0)
-    {
-        pointer[total_size - 1] = 0;
-        total_size--;
-    }
-    return (pointer);
-}
 
 size_t	ft_strlen(const char *s)
 {
 	size_t	i;
 
-	if(s == NULL)
+	if (s == NULL)
 		return (0);
 	i = 0;
 	while (s[i])
@@ -40,7 +33,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	if (s == NULL)
 		return (NULL);
 	if (start >= ft_strlen(s))
-		return (ft_calloc(1,1));
+		return (ft_calloc (1, 1));
 	if (len > ft_strlen(s) - start)
 		len = ft_strlen(s) - start;
 	new = ft_calloc(sizeof(char), (len + 1));
@@ -55,23 +48,24 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	new[i] = '\0';
 	return (new);
 }
-char *ft_strjoin(char const *s1, char const *s2)
+
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-    char *new;
-    size_t len1;
-    size_t len2;
+	char	*new;
+	size_t	len1;
+	size_t	len2;
 	size_t	i;
 
 	i = 0;
-    if (s1 == NULL && s2 == NULL)
-        return (NULL);
-    if (s1 == NULL)
-        return (ft_strdup(s2));
-    if (s2 == NULL)
-        return (ft_strdup(s1));
-    len1 = ft_strlen(s1);
-    len2 = ft_strlen(s2);
-    new = (char *)ft_calloc((len1 + len2 + 1), sizeof(char));
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
+	if (s1 == NULL)
+		return (ft_strdup(s2));
+	if (s2 == NULL)
+		return (ft_strdup(s1));
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	new = (char *)ft_calloc((len1 + len2 + 1), sizeof(char));
 	while (i < (len1 + len2))
 	{
 		while (i < len1)
@@ -86,7 +80,7 @@ char	*ft_strchr(const char *s, int c)
 	char	*ptr;
 
 	if (!s)
-		return (0);	
+		return (0);
 	ptr = (char *)s;
 	while (*ptr != '\0')
 	{
@@ -98,10 +92,12 @@ char	*ft_strchr(const char *s, int c)
 		return (ptr);
 	return (NULL);
 }
+
 char	*ft_strdup(const char *s1)
 {
 	int		i;
 	char	*new;
+
 	i = 0;
 	if (!s1)
 		return (NULL);
